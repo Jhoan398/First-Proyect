b4a9daa623968f9a98cd7462da6b39df1f853599
Jhoan Saldarriaga <ale.xo64@hotmail.com>
2023-05-19 10:43:40 -0500

    Validators on DTO'S

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,4 +1,5 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty, MaxLength } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
@@ -6,19 +7,30 @@ export class CreateUserInput {
   @Field({ nullable: true })
   id: number;
 
+  @MaxLength(10)
+  @IsNotEmpty({
+    message: 'Frist Name is required'
+  })
   @Field({ nullable: false })
   firstName: string;
-
+  
+  @MaxLength(15)
+  @IsNotEmpty({
+    message: 'Last Name is required'
+  })
   @Field({ nullable: false })
   lastName: string;
 
+  @IsNotEmpty()
   @Field({ nullable: false })
   email: string;
 
+  @MaxLength(9)
+  @IsNotEmpty()
   @Field({ nullable: false })
   password: string;
 
-  @Field({ nullable: false })
-  isActive: boolean;
+  @Field({ nullable: true, defaultValue: true })
+  isActive?: boolean;
 
 }
